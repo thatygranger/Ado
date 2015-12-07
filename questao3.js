d83874bfd6d1a49cc60aae36ee912bd5c9ea4b3a
a\kira <akiraichikawa@live.jp>
2015-12-07 14:44:29 -0200

    Update questao3.js

--- questao3.js
+++ questao3.js
@@ -1,3 +1,15 @@
+/*
+- Criar uma função que retorna a quantidade de Memória RAM da máquina
+
+Fora da função, se o valor da memória for menor que 2gb e a máquina for do tipo laptop, 
+adicionar no relatório que o laptop está fora do perfil de hardware da empresa. 
+Caso seja maior que 2gb, imprimir no relatório que a máquina está aderente ao perfil de hardware da empresa.
+
+Fora da função, se o valor da memória for menor que 4gb e a máquina for do tipo desktop, 
+adicionar no relatório que o desktop está fora do perfil de hardware da empresa. 
+Caso seja maior que 4gb, imprimir no relatório que a máquina está aderente ao perfil de hardware da empresa.
+*/
+
 var objWMIService = GetObject( "winmgmts://./root/cimv2" )
 var colItems = objWMIService.ExecQuery( "Select * from Win32_ComputerSystem", null , 48 )
 var colProps = new Enumerator(colItems);
